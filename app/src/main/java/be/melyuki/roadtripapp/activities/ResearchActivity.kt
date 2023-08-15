c1da3b835d2c3bce865c1544f5bd71fdb76cadb9
melvin taider <tmneo1989@hotmail.com>
2023-08-15 15:56:49 +0200

    fabs_simple_update

--- app/src/main/java/be/melyuki/roadtripapp/activities/ResearchActivity.kt
+++ app/src/main/java/be/melyuki/roadtripapp/activities/ResearchActivity.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.commit
 import be.melyuki.roadtripapp.R
 import be.melyuki.roadtripapp.databinding.ActivityResearchBinding
 import be.melyuki.roadtripapp.fragments.MapFragment
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.internal.ViewUtils.showKeyboard
 
 class ResearchActivity : AppCompatActivity() {
 
@@ -31,45 +31,52 @@ class ResearchActivity : AppCompatActivity() {
         binding.tvFabRoadtrip.visibility = View.GONE
 
         binding.fabOptionBtn.setOnClickListener { getOptionMenu() }
+
     }
 
     private fun getOptionMenu() {
         if(!fabsVisible) {
-
-            binding.fabLikeBtn.show()
-            binding.fabDistanceBtn.show()
-            binding.fabPoiBtn.show()
-            binding.fabWeatherBtn.show()
-            binding.fabRoadtripBtn.show()
-
-            binding.tvFabLike.visibility = View.VISIBLE
-            binding.tvFabDistance.visibility = View.VISIBLE
-            binding.tvFabWeather.visibility = View.VISIBLE
-            binding.tvFabPoi.visibility = View.VISIBLE
-            binding.tvFabRoadtrip.visibility = View.VISIBLE
-
-            binding.fabOptionBtn.setImageDrawable(this.getDrawable(R.drawable.fab_icon_arrow_up))
-
-            fabsVisible = true
+            showFabs()
         }
         else {
-
-            binding.fabLikeBtn.hide()
-            binding.fabDistanceBtn.hide()
-            binding.fabPoiBtn.hide()
-            binding.fabWeatherBtn.hide()
-            binding.fabRoadtripBtn.hide()
-
-            binding.tvFabLike.visibility = View.GONE
-            binding.tvFabDistance.visibility = View.GONE
-            binding.tvFabWeather.visibility = View.GONE
-            binding.tvFabPoi.visibility = View.GONE
-            binding.tvFabRoadtrip.visibility = View.GONE
-
-            binding.fabOptionBtn.setImageDrawable(this.getDrawable(R.drawable.fab_icon_arrow))
-
-            fabsVisible = false
+            hideFabs()
         }
+    }
+    private fun showFabs() {
+
+        binding.fabLikeBtn.show()
+        binding.fabDistanceBtn.show()
+        binding.fabPoiBtn.show()
+        binding.fabWeatherBtn.show()
+        binding.fabRoadtripBtn.show()
+
+        binding.tvFabLike.visibility = View.VISIBLE
+        binding.tvFabDistance.visibility = View.VISIBLE
+        binding.tvFabWeather.visibility = View.VISIBLE
+        binding.tvFabPoi.visibility = View.VISIBLE
+        binding.tvFabRoadtrip.visibility = View.VISIBLE
+
+        binding.fabOptionBtn.setImageDrawable(this.getDrawable(R.drawable.fab_icon_arrow_up))
+
+        fabsVisible = true
+    }
+    private fun hideFabs() {
+
+        binding.fabLikeBtn.hide()
+        binding.fabDistanceBtn.hide()
+        binding.fabPoiBtn.hide()
+        binding.fabWeatherBtn.hide()
+        binding.fabRoadtripBtn.hide()
+
+        binding.tvFabLike.visibility = View.GONE
+        binding.tvFabDistance.visibility = View.GONE
+        binding.tvFabWeather.visibility = View.GONE
+        binding.tvFabPoi.visibility = View.GONE
+        binding.tvFabRoadtrip.visibility = View.GONE
+
+        binding.fabOptionBtn.setImageDrawable(this.getDrawable(R.drawable.fab_icon_arrow))
+
+        fabsVisible = false
     }
 
     private fun loadMapFrag() {
