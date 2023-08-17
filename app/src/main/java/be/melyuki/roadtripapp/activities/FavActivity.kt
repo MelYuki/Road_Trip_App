c5747b2bdfb825c360adab18a646d0ed45222ef0
melvin taider <tmneo1989@hotmail.com>
2023-08-17 14:28:59 +0200

    FAVORITES_Design_RecyclerView_&_Adapter

--- app/src/main/java/be/melyuki/roadtripapp/activities/FavActivity.kt
+++ app/src/main/java/be/melyuki/roadtripapp/activities/FavActivity.kt
@@ -2,11 +2,34 @@ package be.melyuki.roadtripapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import be.melyuki.roadtripapp.R
+import be.melyuki.roadtripapp.adapters.ListFavAdapter
+import be.melyuki.roadtripapp.databinding.ActivityFavBinding
+import be.melyuki.roadtripapp.models.MapResearchModel
 
 class FavActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityFavBinding
+
+    private lateinit var favCities : MutableList<MapResearchModel>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fav)
+
+        binding = ActivityFavBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // region RecyclerView
+        favCities = mutableListOf()
+
+        val adapter : ListFavAdapter = ListFavAdapter(this, favCities)
+        binding.rvFavCities.adapter = adapter
+        binding.rvFavCities.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        // endregion
     }
 }
---
+++ app/src/main/java/be/melyuki/roadtripapp/adapters/ListFavAdapter.kt
@@ -0,0 +1,53 @@
+package be.melyuki.roadtripapp.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import be.melyuki.roadtripapp.R
+import be.melyuki.roadtripapp.models.MapResearchModel
+
+class ListFavAdapter(val context : Context, val cities : List<MapResearchModel>) :
+    RecyclerView.Adapter<ListFavAdapter.CityViewHolder>() {
+    class CityViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvTitle : TextView
+        private val tvLon : TextView
+        private val tvLat : TextView
+
+        fun setTitle(name : String) {
+            tvTitle.text = name
+        }
+        fun setLon(name : String) {
+            tvLon.text = name
+        }
+        fun setLat(name : String) {
+            tvLat.text = name
+        }
+
+        init {
+            tvTitle = itemView.findViewById(R.id.rv_item_city_display_name)
+            tvLon = itemView.findViewById(R.id.rv_item_city_lon)
+            tvLat = itemView.findViewById(R.id.rv_item_city_lat)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+        val view : View = LayoutInflater.from(context).inflate(R.layout.item_rv_fav, parent, false)
+        return CityViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return cities.size
+    }
+
+    override fun onBindViewHolder(holder: CityViewHolder, position: Int) {
+        val element : MapResearchModel = cities[position]
+
+        holder.setTitle(element.displayName.toString())
+        holder.setLon(element.lon.toString())
+        holder.setLat(element.lat.toString())
+    }
+}
